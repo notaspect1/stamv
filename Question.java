3bcf6f5446eae78be1e91b1a42b031cdfed679a0
Kaden <81715956+EnchanterKC@users.noreply.github.com>
2021-05-23 15:59:56 -0700

    Update Question.java

--- Question.java
+++ Question.java
@@ -4,18 +4,49 @@ class Question{
     private var question = "";
     private var responseList = [];
     private var correctAnswerIndexes = [];
+    private var selectedList = [];
+    
     
     constructor(){
-      response.push(); 
+      
     }
   
-    constructor(quizLength){
-      for(var i =0; i<quizLength; i++){
-        questionList.push(new Question());
-      }
+    constructor(question, responseList){
+      this.question = question;
+      this.responseList = responseList;
     }
     
-   get 
+   get currentQuestion(){
+     return question; 
+     
+   }
+   set currentQuestion(newQuestion){
+     question = newQuestion;
+   }
+   get response(){
+     return responseList;
+   }
+   get selected(){
+     return selectedList;
+     
+   }
+  get correctAnswer(){
+    return correctAnswerIndexes;
+  }
+  
+   public isCorrect(){
+     if(correctAnswerIndexes.length != selectedList.length){
+       return false;
+     }
+     for(var i =0; i<correctAnswerIndexes.length; i++){
+       if(correctAnswerIndexes[i] != selectedList[i]){
+         return false;
+       }
+     }
+     return true;
+   }
+  
+   
   
   
 }
