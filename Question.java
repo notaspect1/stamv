ddad8d09becd7f7dc2ddf437e1d7f9b7d5484787
Kaden <81715956+EnchanterKC@users.noreply.github.com>
2021-05-23 15:41:13 -0700

    Update Question.java

--- Question.java
+++ Question.java
@@ -1,1 +1,21 @@
 
+class Question{
+  
+    private var question = "";
+    private var responseList = [];
+    private var correctAnswerIndexes = [];
+    
+    constructor(){
+      response.push(); 
+    }
+  
+    constructor(quizLength){
+      for(var i =0; i<quizLength; i++){
+        questionList.push(new Question());
+      }
+    }
+    
+   get 
+  
+  
+}
