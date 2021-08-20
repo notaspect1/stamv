23c85d17e867d6d745347cb9eed9937767e40f7e
Atli-A <atli.nikulas@gmail.com>
2021-08-19 19:05:41 -0700

    Rewrote how creating and removing elements works
    This means that deleting questions now works. Additionally, they now keep
    their values across additions or removals of elements.

--- createquiz/createquiz.js
+++ createquiz/createquiz.js
@@ -23,7 +23,7 @@ document.getElementById("quizDescription").value = Description
 document.getElementById("visiblityDropdown").value = Visibility
 
 
-var addQuestionTemplate =`      
+let addQuestionTemplate = `      
 <div id="question${amount}" class="questions">
     <h3>Question ${amount}</h3>
     <label class="fakeLabel" for="term${amount}">Term</label> 
@@ -36,26 +36,73 @@ var addQuestionTemplate =`
 </div>
 `;
 
+let questionsList = []
+
 /* 
 
 Fill in already written questions from fetched data here using a for loop it should set the amount variable as well
 
 */
 
-document.getElementById("addQuestion").onclick = function(){
+
+//add Question
+document.getElementById("addQuestion").onclick = function () {
+    refillList()
     amount++
-    addQuestionTemplate =`      
-    <div id="question${amount}" class="questions">
-        <h3>Question ${amount}</h3>
-        <label class="fakeLabel" for="term${amount}">Term</label> 
-        <input type="text" name="quizName" id="quizName">
-        <button class="deleteButton">Delete</button>
+    addQuestionTemplate = `      
+    <div id="question/num/" class="questions">
+        <h3>Question /num/</h3>
+        <label class="fakeLabel" for="term/num/">Term</label> 
+        <input type="text" name="term/num/" id="term/num/">
+        <button class="deleteQuestion" onclick="removeQuestion(/num/)">Delete</button>
         <div class="definitions">
-            <label class="fakeLabel" for="definition${amount}">Definition (optional)</label> 
-            <textarea type="text" class="definition" name="definition${amount}" id="definition${amount}"></textarea>
+            <label class="fakeLabel" for="definition/num/">Definition (optional)</label> 
+            <textarea type="text" class="definition" name="definition/num/" id="definition/num/"></textarea>
         </div> 
     </div>
     `;
 
-    document.getElementById("listOfQuestions").innerHTML += addQuestionTemplate
+    questionsList.push(addQuestionTemplate);
+    render()
+}
+
+//remove question
+function removeQuestion(pos) {
+    refillList()
+    //console.log("removing question")
+    questionsList.splice(pos, 1)
+    render()
+}
+
+function refillList() {
+    let term = ""
+    let definition = ""
+    for (let i = 0; i < questionsList.length; i++) {
+        term = document.getElementById(`term${i}`).value.trim()
+        definition = document.getElementById(`definition${i}`).value.trim()
+        console.log(definition)
+        questionsList[i] = `
+        <div id="question/num/" class="questions">
+        <h3>Question /num/</h3>
+        <label class="fakeLabel" for="term/num/">Term</label> 
+        <input type="text" name="term/num/" id="term/num/" value="${term}">
+        <button class="deleteQuestion" onclick="removeQuestion(/num/)">Delete</button>
+        <div class="definitions">
+            <label class="fakeLabel" for="definition/num/">Definition (optional)</label> 
+            <textarea type="text" class="definition" name="definition/num/" id="definition/num/">${definition}
+            </textarea>
+        </div> 
+        </div>
+        `
+    }
+}
+
+function render() {
+    let buffer = ""
+    for (let i = 0; i < questionsList.length; i++) {
+        buffer += (questionsList[i].replaceAll("/num/", i)) //I know replaceAll is relatively recent maybe we should use a more outdated method
+        //console.log(buffer)
+    }
+    document.getElementById("listOfQuestions").innerHTML = buffer;
+    // console.log(questionsList)
 }
