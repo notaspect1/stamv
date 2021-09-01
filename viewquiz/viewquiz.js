fe7085c1c67e942a20391b53ebc2979e97a89d03
Atli-A <atli.nikulas@gmail.com>
2021-08-31 17:59:24 -0700

    Added flashcard switching and fixed fetching.
    Additionally, I added a title, description and visiblity settings.

--- viewquiz/viewquiz.js
+++ viewquiz/viewquiz.js
@@ -1,15 +1,44 @@
-let terms = []
-let definitions = []
 
+let questions = []
+let currentPosition = 0;
 let url = "./test.json"
 
-json = await fetch(url, {method: 'GET',})
-.then(function(response) {
-    return response.json();
-  })
-console.log(json)
-//   .then( res => res.json())
-//   .then( data => console.log(data));
+
+fetch(url, {method: 'GET',})
+  .then(Response => Response.json())
+  .then(data => doStuff(data))
+
+function doStuff(data) {
+  questions = data.questions
+  console.log(data)
+  cardUpdate()
+}
+
+function cardUpdate() {
+  currentPosition = Math.max(Math.min(questions.length - 1, currentPosition), 0)
+  document.getElementById("displayTerm").innerText = questions[currentPosition].term
+  document.getElementById("displayDefinition").innerText = questions[currentPosition].definitions[0]
+}
 
 
-document.getElementById("displayTerm").innerText = "test"
+document.getElementById("leftArrow").classList.add("disabled")
+
+
+//button onclicks
+document.getElementById("rightArrow").onclick = function() {
+  currentPosition++;
+  if (currentPosition == questions.length - 1) {
+    document.getElementById("rightArrow").classList.add("disabled")
+  }
+  document.getElementById("leftArrow").classList.remove("disabled")
+  cardUpdate()
+}
+
+document.getElementById("leftArrow").onclick = function() {
+  currentPosition--;
+  if (currentPosition == 0) {
+    document.getElementById("leftArrow").classList.add("disabled")
+  }
+  document.getElementById("rightArrow").classList.remove("disabled")
+  cardUpdate()
+}
