667e5165c2baa6204364dd18652099c717125cfd
Atli-A <atli.nikulas@gmail.com>
2021-09-25 15:40:45 -0700

    The answers and question are now generated.
    It currently only chooses the first question right now. The right answer
    is always listed and not hard coded.

--- viewquiz/viewquiz.js
+++ viewquiz/viewquiz.js
@@ -13,10 +13,37 @@ function doStuff(data) {
   questions = data.questions
   document.getElementById("title").innerText = data.title
   document.getElementById("description").innerText = data.description
-  console.log(data)
+  console.log(questions)
+  generate(questions[0])
+}
+
+function generate(correctQuestion) {
+  arr = []
+  arr.push(correctQuestion.definitions[0])
+  for (let i = 0; i < numAnswers - 1; i++) {
+    x = parseInt(Math.random() * questions.length)
+    arr.push(questions[x].definitions[0])
+  }
+
+  buffer = ""
+  for (let i = 0; i < arr.length; i++) {
+    buffer += `
+    <div class="answers" onclick="makeSelected(this)">
+      <p>
+      REPLACE
+      </p>
+    </div>
+    `.replace("REPLACE", arr[i])
+  }
+
+  document.getElementById("term").innerText = correctQuestion.term
+  document.getElementById("answersContainer").innerHTML = buffer;
 }
 
 function makeSelected(id) {
+  if (selectedAnswer != null) {
+    selectedAnswer.classList.remove("selected")
+  }
   selectedAnswer = id
   if (id.classList.contains("selected")) {
     id.classList.remove("selected")
